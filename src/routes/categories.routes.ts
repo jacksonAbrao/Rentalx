16a3f85c41a87d64a0304197b7c4277155130545
Jackson Abrão <jackson.comp1681@hotmail.com>
2021-08-14 18:10:27 -0300

    continuing api

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 
-import { PostgressCategoriesRepository } from "../modules/cars/repositories/PostgresCategoriesRepositories";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new PostgressCategoriesRepository();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
